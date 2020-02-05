4c7595fb0d91dc6e065d7469b56fcd5cae92139e
iangYo <iangabrielrodrigues@gmail.com>
2020-02-04 21:20:54 -0300

    rota de callback adicionada

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,8 @@
 const routes = require('express').Router();
 const authController = require('../controllers/authController');
 
-routes.get('/', (req, res) => {
-	return res.json({ ok: false });
-});
-
 routes.get('/index', authController.index);
-routes.get('/auth', authController.auth);
+routes.get('/show', authController.show);
+routes.get('/callback', authController.callback);
 
 module.exports = routes;
